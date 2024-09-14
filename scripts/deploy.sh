ac1e685da76655e0f0c858aec11b5fe4bda66902
Chethaka Lakshitha <chethaka.sl@gmail.com>
2024-09-14 15:48:00 +0530

    Trigger deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,8 +13,17 @@
 #   ./scripts/deploy.sh
 #
 
+#!/bin/bash
+
 set -u # or set -o nounset
+
+# Set these variables explicitly
+CONTAINER_REGISTRY="assessment7acr.azurecr.io"
+VERSION="latest"
+
+# Ensure that the variables are set
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
+# Substitute the variables in the YAML and apply the configuration to Kubernetes
 envsubst < ./scripts/kubernetes/deploy.yaml | kubectl apply -f -
